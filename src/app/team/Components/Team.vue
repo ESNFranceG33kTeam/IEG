cf849fdc1dfd8876208b60a38aa5a5a873d0b56a
Rémy Raes <raes.remy@gmail.com>
2019-01-07 19:37:48 +0100

    [feat] updating "Team" component content

--- src/app/team/Components/Team.vue
+++ src/app/team/Components/Team.vue
@@ -23,65 +23,189 @@
       <vue-grid-row>
 
         <vue-grid-item class="vueGridItem">
-          <h2>Nos héros</h2>
+          <h3></h3>
 
           <vue-panel>
-            <vue-panel-header title="Michel Gérard" subtitle="Head of ze OC"
+            <vue-panel-header title="Julien Guillot" subtitle="Head of the OC"
                               image="https://avatars2.githubusercontent.com/u/1667598?s=460&v=4" />
             <vue-panel-body>
-              Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut
-              labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
-              et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
-              Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut
-              labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
-              et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+              He’s Julien , 25, journalist (LOL). He likes when his team takes initiative, but he’s always there to help
+              them go further. When there’s a problem you can count on him to help and solve it. He likes to be centre
+              stage to protect his team and spare a waste of energy (a real hero, this man!). Il aime les potatoes et
+              possède 2 cochons d’inde.<br/>
+              Fun Fact: He’s proud of his selfie with Zlatan Ibrahimović and he has commented on the radio for the
+              Football Euro 2016.
+
             </vue-panel-body>
           </vue-panel>
         </vue-grid-item>
 
         <vue-grid-item class="vueGridItem">
-          <h2>Nos héros</h2>
+          <h3></h3>
           <vue-panel>
-            <vue-panel-header title="Thierry Jacky" subtitle="Trézorier of ze OC"
+            <vue-panel-header title="Julie Nevant" subtitle="Vice-Head of the OC"
                               image="https://avatars2.githubusercontent.com/u/1667598?s=460&v=4" />
             <vue-panel-body>
-              Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut
-              labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
-              et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
-              Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut
-              labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
-              et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+              She is 22 and she just finished her Master Degree of Bioethics Law.<br/>
+              She’s a fan Olympic Games, she has been swimming for 10 years and now she can go toe to toe with a mermaid.
+              She loves sports and that’s why the IEG are so important to her. From a match to another she will make
+              sure that the program of every match of the IEG will run as efficiently as possible, coordinating with
+              the different teams of volunteers.
+
             </vue-panel-body>
           </vue-panel>
         </vue-grid-item>
 
         <vue-grid-item class="vueGridItem">
-          <h2>Nos héros</h2>
+          <h3></h3>
           <vue-panel>
-            <vue-panel-header title="Horan Didier" subtitle="Respo com of ze OC"
+            <vue-panel-header title="Yassine Berrada" subtitle="Partnership Manager"
                               image="https://avatars2.githubusercontent.com/u/1667598?s=460&v=4" />
             <vue-panel-body>
-              Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut
-              labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
-              et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
-              Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut
-              labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
-              et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+              He is 23 years old and has been a volunteer for 3 years in ESN Montpellier. He’s doing a Masters in
+              Business Development and Key account Management after getting a masters in Engineering. He just recently
+              joined Esn Paris after moving to the capital for his studies. He is a dynamic, energetic and cheerful
+              person. He likes teamwork and helping others to achieve their goals.
+            </vue-panel-body>
+          </vue-panel>
+        </vue-grid-item>
+
+        <vue-grid-item class="vueGridItem">
+          <h3></h3>
+          <vue-panel>
+            <vue-panel-header title="Fadile Bhayat" subtitle="CHARGÉ DES RELATIONS PUBLIQUES ET DES PARTENAIRES PUBLIQUES"
+                              image="https://avatars2.githubusercontent.com/u/1667598?s=460&v=4" />
+            <vue-panel-body>
+              In ESN for four years now, he has been local representative and president of ESN Paris. He is also very
+              invested at the national level as an administrator in the board of directors. Lately, he has been working
+              on the creation ESN Paris’ magazine. Quite the multi-tasker, he studies Information and Communications.
+              Beforehand, he also did Computing and Law.
+            </vue-panel-body>
+          </vue-panel>
+        </vue-grid-item>
+
+        <vue-grid-item class="vueGridItem">
+          <h3></h3>
+          <vue-panel>
+            <vue-panel-header title="Orane Maquin" subtitle="Communication Manager"
+                              image="https://avatars2.githubusercontent.com/u/1667598?s=460&v=4" />
+            <vue-panel-body>
+              She is 22, she’s the boss of Total. She first joined ESN in Lille in january 2016, before moving to Paris
+              in september 2017, where she joined her beloved team of parisians and never left. She is a member of
+              French ComCom and candidate member of international ComCom. Being part of these communications team helped
+              here develop great skills, and she hopes to keep going further with beautiful projects such as the IEG!
+            </vue-panel-body>
+          </vue-panel>
+        </vue-grid-item>
+
+        <vue-grid-item class="vueGridItem">
+          <h3></h3>
+          <vue-panel>
+            <vue-panel-header title="Damon Singleton" subtitle="Language Auditor, CHARGÉ DES RELATIONS INTERNATIONALES"
+                              image="https://avatars2.githubusercontent.com/u/1667598?s=460&v=4" />
+            <vue-panel-body>
+              He’s Damon, born in the USA. He spent the most of his studies in France. He has a Masters degree in Law,
+              specializing in the Protection of rights and fundamental freedom. He’s the son of a basketball coach and
+              today an NBA sports commentator for BeIn Sports. He’s a real fan of Basketball which he has played for 11
+              years in a club. He’s also a film lover, globe-trotter and oenologist during his spare time.<br/>
+              Fun fact: Loves animals, but loves barbecues even more.
+
+            </vue-panel-body>
+          </vue-panel>
+        </vue-grid-item>
+
+        <vue-grid-item class="vueGridItem">
+          <h3></h3>
+          <vue-panel>
+            <vue-panel-header title="Philippine Ramirez" subtitle="Treasurer"
+                              image="https://avatars2.githubusercontent.com/u/1667598?s=460&v=4" />
+            <vue-panel-body>
+              She’s Philippine, almost 25 years old She has a masters degree in Accounting and Auditing, and guess what,
+              she was treasurer of ESN France and she’s working as Financial Controller in Paris. She has been a
+              volunteer in ESN for 4 years and now, she’s an alumnus but always willing to help the network. She loves
+              exploring the world and travelling.  She is a dynamic person, always attentive and looking for new
+              challenges!<br/>
+              Fun Fact: She often loses her voice during events, but she always gives everything!
+
+            </vue-panel-body>
+          </vue-panel>
+        </vue-grid-item>
+
+        <vue-grid-item class="vueGridItem">
+          <h3></h3>
+          <vue-panel>
+            <vue-panel-header title="Samia Hanki" subtitle="Head of Sports"
+                              image="https://avatars2.githubusercontent.com/u/1667598?s=460&v=4" />
+            <vue-panel-body>
+              And here is Samia, 23, has just finished her Master degree in Cultural Tourism. A little newbie at ESN,
+              she has a strong desire to create new experiences and contribute to the development of this association.
+              She’s from Italy and she likes to socialize with other cultures. That’s why she joined ESN and wants to
+              help with the organisation of this IEG.<br/>
+              Fun Fact: she’s little, but she has already played rugby for one year (without having a bone broken).
+
+            </vue-panel-body>
+          </vue-panel>
+        </vue-grid-item>
+
+        <vue-grid-item class="vueGridItem">
+          <h3></h3>
+          <vue-panel>
+            <vue-panel-header title="Rémy Raes" subtitle="Webmaster"
+                              image="https://avatars2.githubusercontent.com/u/1667598?s=460&v=4" />
+            <vue-panel-body>
+              Actually studiying computer science and leading the French IT Committee, he's a passionnate geek; but don't
+              be scared of him, he has the ability to speak to other things than computers, as he's been webmaster, local
+              responsible, and vice-president of ESN Lille for the past three years!
+            </vue-panel-body>
+          </vue-panel>
+        </vue-grid-item>
+
+        <vue-grid-item class="vueGridItem">
+          <h3></h3>
+          <vue-panel>
+            <vue-panel-header title="Hrvoje Grganovic" subtitle="Project Coordinator, IEG Committee"
+                              image="https://avatars2.githubusercontent.com/u/1667598?s=460&v=4" />
+            <vue-panel-body>
+              He’s passionate volunteer and since he joined this Network he can truly say he became a European citizen.
+              He enjoy advocating for a healthier lifestyle of the students and volunteers but he also do not shy away
+              from sport himself as amateur football enthusiast. He jumped on board of the IEG train years back as a
+              National Representative of ESN Croatia and since then have not looked back.
+            </vue-panel-body>
+          </vue-panel>
+        </vue-grid-item>
+
+        <vue-grid-item class="vueGridItem">
+          <h3></h3>
+          <vue-panel>
+            <vue-panel-header title="Aleksandra Seweryn" subtitle="Community Manager, IEG Committee"
+                              image="https://avatars2.githubusercontent.com/u/1667598?s=460&v=4" />
+            <vue-panel-body>
+              Some people call her Ola, others Aleksandra, the rest make fun of her name saying Hola Ola! She is a
+              former NR of ESN Poland, truly passionate about the International Erasmus Games which was the first
+              project she got involved in. She’s in love with travels, Italy, French language and her cute Golden
+              Retriever and she is really looking forward to take care of IEG community this year! <strong>And she loves
+              Julien, the head of the OC.</strong>
+
+            </vue-panel-body>
+          </vue-panel>
+        </vue-grid-item>
+
+        <vue-grid-item class="vueGridItem">
+          <h3></h3>
+          <vue-panel>
+            <vue-panel-header title="Zrinka Trusic" subtitle="Communication Officer, IEG Committee"
+                              image="https://avatars2.githubusercontent.com/u/1667598?s=460&v=4" />
+            <vue-panel-body>
+              After developing her skills in ESN Zadar, ESN Croatia and NEC, this is her fourth gig in the field of
+              communications and she is looking forward to new challenges. Handball is her greatest love when it comes
+              to sport, but she have tried to build a professional career in table tennis. She spends most of her time
+              reading, trying to learn new languages, DIY-ing and adding things to her bucket list.
             </vue-panel-body>
           </vue-panel>
         </vue-grid-item>
       </vue-grid-row>
 
-      <vue-grid-row>
-
-        <vue-grid-item class="vueGridItem">
-          <h2>L'équipe</h2>
-          <vue-carousel :images="images" />
-          <h6></h6>
-        </vue-grid-item>
-      </vue-grid-row>
-
-      <vue-grid-row></vue-grid-row>
+      <h3></h3>
 
     </vue-grid>
   </div>

980d09d4a359daf9fd1d682ef72be6b76d9c9f3e
Rémy Raes <raes.remy@gmail.com>
2019-01-31 15:00:08 +0100

    [feat] adding content to candidacy component

--- src/app/candidacy/Components/Candidacy.vue
+++ src/app/candidacy/Components/Candidacy.vue
@@ -7,12 +7,13 @@
           <vue-grid-item class="vueGridItem">
             <h1>{{ $t('App.nav.candidacy') }}</h1>
             <p>
-              Lorem ipsum dolor sit amet, consetetur sadipscing elitr.
+                ESN Paris wishes to participate in the development of a sports project for international students.
             </p>
-            <p>
-              At vero eos et accusam et justo duo dolores
-              et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
-            </p>
+              <p>
+                The aim is to create throughout their stay, an event vector of profound and sustainable values, in
+                several domains such as health, well-being, social cohesion, education, solidarity and the respect of
+                the environment.
+              </p>
           </vue-grid-item>
         </vue-grid-row>
       </vue-grid>
@@ -22,11 +23,21 @@
 
       <vue-grid-row>
           <vue-grid-item class="vueGridItem">
-              <h2>Disability</h2>
+              <h2>Disability: Sports for everyone</h2>
 
               <vue-panel>
                   <vue-panel-body>
-                      bla bla bla
+                      The main topics of these IEG will be “<strong>sports for everyone</strong>” and
+                      “<strong>Handisport</strong>”. With displays for disability open to all local people during the
+                      weekend, we want to show that being in a handicap situation is not a fatality, and that a sport
+                      activity can be practiced in high level. With the presence of handisport champions during the whole
+                      competition, the topic of handicap will be present during the 3 days of the event, and we hope that
+                      we will be able to raise awareness on this matter. Finally, we want to shed the light on the work
+                      provided by many volunteers during the whole year, and that echoes only during the paralympic games,
+                      once every four years. Handisport was our priority when we applied and it will be at the heart of
+                      the project.
+                      <h3></h3>
+                      <img src="/candidacy/way.png" alt="a student with a wheelchair"/>
                   </vue-panel-body>
               </vue-panel>
           </vue-grid-item>
@@ -38,7 +49,15 @@
 
                 <vue-panel>
                     <vue-panel-body>
-                        bla bla bla
+                        As young European citizen, it is our duty to take part in the commitments for a better future.
+                        The United Nations and European Union have adopted <strong>17 sustainable development
+                        goals</strong>, and ESN international has targeted <strong>6 causes</strong> to support. It is
+                        in this perspective of enhancing the importance of <strong>health and well-being</strong> that
+                        the IEG have been created among ESN. Hence, we consider it our mission to make it an outstanding
+                        example of a <strong>sustainable engaging event</strong>.
+                        As much as possible, we are encouraging all of our participants and volunteers to be the most
+                        environnement respectful : to conserve water, to reduce packing materials, to reduce use of
+                        plastic and paper, to limit transportation, to recycle and limit waste.
                     </vue-panel-body>
                 </vue-panel>
             </vue-grid-item>
@@ -50,7 +69,32 @@
 
                 <vue-panel>
                     <vue-panel-body>
-                        bla bla bla
+                        Sport is often an issue during an international stay or an Erasmus exchange. Would we have enough
+                        time to practise a sport ? Who should I contact ? What to do ?<br/>
+                        Through an <strong>exhibition</strong> of several organisations, we are going to show that it is
+                        possible to stay in shape even abroad. Key point of the Erasmus program, sport is also a way to
+                        <strong>fight against intolerance and discrimination</strong>, two topics our organisation really
+                        cares about.
+                    </vue-panel-body>
+                </vue-panel>
+            </vue-grid-item>
+        </vue-grid-row>
+
+        <vue-grid-row>
+            <vue-grid-item class="vueGridItem">
+                <h2>European openness</h2>
+
+                <vue-panel>
+                    <vue-panel-body>
+                        The European question will also be at the heart of the year 2019 with elections coming by May and
+                        Brexit. For the first time in France and in Paris, one of the greatest european creation,
+                        <strong>the Erasmus program</strong>, will be brought to light by a sport competition at an
+                        international level. Through this event, our goal is to show that there aren’t citizens from
+                        different european countries but <strong>ONE european citizenship defined by common values</strong>.
+                        Is there a better way than sport to gather people? In France, founder country of Europe, it is
+                        an important message to raise.
+                        <h3></h3>
+                        <img src="/candidacy/flag.png" alt="students raising flags from all european countries"/>
                     </vue-panel-body>
                 </vue-panel>
             </vue-grid-item>
